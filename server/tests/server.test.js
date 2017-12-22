7be2bb313384eb58e07c79103847a62e0001bc60
marcio <marciodscampos@gmail.com>
2017-12-22 10:57:16 -0200

    created GET todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,12 +3,16 @@ const request = require('supertest');
 const {app} = require('../server');
 const {Todo} = require('../models/todo');
 
+const todos = [{text: 'first note'}, {text: 'second note'}];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 })
 
 describe('POST /todos', () => {
-    test('It shold create a todo', (done) => {
+    test('it should create a todo', (done) => {
         var text = 'Test todo text';
         
         const response = request(app)
@@ -21,7 +25,7 @@ describe('POST /todos', () => {
             .end((err, res) => {
                 if(err) return done(err);
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -29,7 +33,7 @@ describe('POST /todos', () => {
             });
     });
 
-    test('Shold not create a todo with invalid body data', (done) => {
+    test('it should not create a todo with invalid body data', (done) => {
         request(app)
             .post('/todos')
             .send({})
@@ -38,9 +42,21 @@ describe('POST /todos', () => {
                 if (err) return done(err);
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
-            })
+            });
     });
-})
+});
+
+describe('GET /todos', () => {
+    test('it should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
+});
