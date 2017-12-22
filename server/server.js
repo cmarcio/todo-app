89ee46183b3560bfb9d9b473d349bedbdddb464c
marcio <marciodscampos@gmail.com>
2017-12-22 15:31:16 -0200

    creted route DELETE todo

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const { mongoose } = require('./db/mongoose');
 const { User } = require('./models/user');
 const { Todo } = require('./models/todo');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
@@ -44,8 +45,22 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Started server on port 3000');
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo) return res.status(404).send();
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Started server on port ${port}`);
 });
 
 module.exports = { app };
