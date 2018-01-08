345fc5ce834a2d35f68731c389e9074c734ea880
marcio <marciodscampos@gmail.com>
2018-01-08 11:36:00 -0200

    add POST /user/login rout

--- server/models/user.js
+++ server/models/user.js
@@ -55,22 +55,38 @@ UserSchema.methods = {
     }
 };
 
-UserSchema.statics.findByToken = function(token) {
-    const User = this;
-    let decoded;
+UserSchema.statics = {
+    findByToken: function(token) {
+        const User = this;
+        let decoded;
 
-    try {
-        decoded = jwt.verify(token, 'abc123');
-    } catch (e) {
-        return Promise.reject();
+        try {
+            decoded = jwt.verify(token, 'abc123');
+        } catch (e) {
+            return Promise.reject();
+        }
+
+        return User.findOne({
+            '_id': decoded._id,
+            'tokens.token': token,
+            'tokens.access': 'auth'
+        });
+    },
+    findByCredentials: function(email, password) {
+        const User = this;
+
+        return User.findOne({email}).then((user) => {
+            if(!user) return Promise.reject();
+            
+            return new Promise((resolve, reject) => {
+                bcrypt.compare(password, user.password, (err, res) => {
+                    if(res) resolve(user);
+                    else reject();
+                });
+            });
+        });
     }
-
-    return User.findOne({
-        '_id': decoded._id,
-        'tokens.token': token,
-        'tokens.access': 'auth'
-    });
-};
+}
 
 UserSchema.pre('save', function(next) {
     const user = this;
--- server/server.js
+++ server/server.js
@@ -100,11 +100,20 @@ app.post('/users', (req, res) => {
     });
 });
 
-
-
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
-})
+});
+
+app.post('/users/login', (req, res) => {
+    const {email, password} = _.pick(req.body, ['email', 'password']);
+    User.findByCredentials(email, password).then((user) => {
+        user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(port, () => {
     console.log(`Started server on port ${port}`);
